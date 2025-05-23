a41105fd5dc54c4563a9ce63504327b629dff99e
Peter Licari <prlicari13@gmail.com>
2025-05-23 16:18:01 -0400

    Updated tests

--- tests/testthat/test-logical_comparisons.R
+++ tests/testthat/test-logical_comparisons.R
@@ -1,26 +1,6 @@
-test_that("xor works", {
-  expect_equal(TRUE %x|% TRUE, FALSE)
-  expect_equal(TRUE %x|% FALSE, TRUE)
-  expect_equal(FALSE %x|% TRUE, TRUE)
-  expect_equal(FALSE %x|% FALSE, FALSE)
-  
-  expect_equal(c(TRUE,FALSE,TRUE) %x|% c(TRUE,FALSE,FALSE), c(FALSE,FALSE,TRUE))
-  
-})
 
 
-test_that("nand works", {
-  expect_equal(TRUE %!&% TRUE, FALSE)
-  expect_equal(TRUE %!&% FALSE, TRUE)
-  expect_equal(FALSE %!&% TRUE, TRUE)
-  expect_equal(FALSE %!&% FALSE, TRUE)
-  
-  expect_equal(c(FALSE,TRUE,TRUE) %!&% c(FALSE,FALSE,TRUE), c(TRUE,TRUE,FALSE))
-  
-})
-
-
-test_that("aliases works", {
+test_that("functions work", {
   expect_equal(TRUE %nand% TRUE, FALSE)
   expect_equal(TRUE %nand% FALSE, TRUE)
   expect_equal(FALSE %nand% TRUE, TRUE)
